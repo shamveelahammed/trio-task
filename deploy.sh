301c6ee92b80c5562faea6063703865b662ef268
Ubuntu <ubuntu@ip-172-31-25-86.eu-west-1.compute.internal>
2021-12-09 15:39:01 +0000

    add  deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Create network
+docker network create my-trio-task-network 
+
+
+#Create volume
+docker volume create trio-db-volume
+
+#Building images
+docker build -t my-sql-img db
+docker build -t trio-task-img flask-app
+
+#run db container
+docker run -d  --network my-trio-task-network  --volume trio-db-volume:/var/lib/mysql --name mysql my-sql-img  
+#run flask app
+docker run -d  --network my-trio-task-network --name flask-app  trio-task-img
+#run nginx container
+docker run -d --network my-trio-task-network -p 80:80 --name nginx_container --mount type=bind,source=$(pwd)/nginx/nginx.conf,target=/etc/nginx/nginx.conf nginx
